60c0be3bea57fe20e6323d7a1c08573f9a43cc2e
acostapazo <acosta@alicebiometrics.com>
2020-03-06 17:11:35 +0100

    Update releaser.sh

--- src/releaser.sh
+++ src/releaser.sh
@@ -3,13 +3,20 @@
 releaser::create() {
   local -r version="$1"
   local -r description="$2"
-  local -r branch="$3"
-  local -r draft="$4" == "true"
-  local -r prerelease="$5" == "true"
+  local -r repo="$3"
+  local -r branch="$4"
+  local -r draft="$5" == "true"
+  local -r prerelease="$6" == "true"
 
   release_data=$(printf '{"tag_name": "%s","target_commitish": "%s","name": "%s","body": "%s","draft": %s,"prerelease": %s}' "$version" "$branch" "$version" "$description" "$draft" "$prerelease" )
 
   echo "$release_data"
+  
+  if [ "$repo" == "this" ]; then
+    repo=$GITHUB_REPOSITORY
+  fi
+  
+  echo "$repo"
 
-  github::create_release "$release_data"
+  github::create_release "$release_data" "$repo"
 }
