be360477c6d3fc1c530846161faba521cbd8d30b
acostapazo <acosta@alicebiometrics.com>
2020-03-06 17:08:08 +0100

    Update github.sh

--- src/github.sh
+++ src/github.sh
@@ -1,10 +1,11 @@
 github::create_release() {
   local -r release_data=$1
+  local -r repo=$2
 
   curl -sSL \
   -X POST \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "$GITHUB_API_HEADER" \
   --data "$release_data" \
-   "$GITHUB_API_URI/repos/$GITHUB_REPOSITORY/releases"
+   "$GITHUB_API_URI/repos/$repo/releases"
 }
