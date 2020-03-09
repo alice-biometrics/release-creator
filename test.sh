08d1bb18d022967b04b9c709087ea7451bc884bd
acostapazo <acosta@alicebiometrics.com>
2020-03-09 15:57:17 +0100

    Feature/inherit release description (#3)
    * Add inherit value on description. (Get it from current Release)
    * Add inherit value to version. (Get it from current Release)

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 GITHUB_TOKEN=$1
-version="v2.0.0"
-description="New version"
+version="inherit"
+description="inherit"
 repo="alice-biometrics/github-releaser"
 branch="master"
 draft="false"
 prerelease="false"
 
-docker build --no-cache -t github-releaser.
+docker build --no-cache -t github-releaser .
 
 docker run github-releaser:latest \
 	"$GITHUB_TOKEN" \
